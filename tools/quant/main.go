cf3191353d124f7622d8435d2c48d9738e1e1278
libinmath <799768295@qq.com>
2023-01-11 22:38:01 +0800

    error in gostock

--- tools/quant/main.go
+++ tools/quant/main.go
@@ -1,24 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"gostock"
-)
-
-func main() {
-	// 获取股票价格
-	price, err := gostock.GetPrice("GOOG")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// 使用股票价格历史数据预测未来价格
-	futurePrice, err := gostock.PredictPrice(price)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("预测GOOG股价为%f", futurePrice)
-}
+//import (
+//	"fmt"
+//	"gostock"
+//)
+//
+//func main() { // gostock not found. run error
+//	// 获取股票价格
+//	price, err := gostock.GetPrice("GOOG")
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//
+//	// 使用股票价格历史数据预测未来价格
+//	futurePrice, err := gostock.PredictPrice(price)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//
+//	fmt.Printf("预测GOOG股价为%f", futurePrice)
+//}
